615a04bc1e79c51c57025b0639cfdff3d6d0eba3
vincent <vincent.chauvaux@gmail.com>
2025-02-23 20:24:43 +0100

    Update Header component container classes for better responsiveness

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
         : 'bg-transparent',
     ]"
   >
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="container mx-auto px-4">
       <nav class="flex items-center justify-between py-6" aria-label="Global">
         <div class="flex lg:flex-1">
           <a href="#" class="-m-1.5 p-1.5">
@@ -65,7 +65,7 @@
       <div
         class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
       >
-        <div class="mx-auto max-w-7xl px-6">
+        <div class="container mx-auto px-4">
           <div class="flex items-center justify-between">
             <a href="#" class="-m-1.5 p-1.5">
               <span class="sr-only">Whisker</span>
